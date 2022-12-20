edee40a76088a7167ef242d5e516d0d7fdeb75f6
Sophy Lee <stlee26@gmail.com>
2022-12-19 21:44:58 -0600

    Use after_set_user warden hook to clear session tokens both when users are signed in during registration as well as through the sessions controller, to fix CSRF token fixation attack vulnerability

--- lib/devise/hooks/csrf_cleaner.rb
+++ lib/devise/hooks/csrf_cleaner.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-Warden::Manager.after_authentication do |record, warden, options|
+Warden::Manager.after_set_user do |record, warden, options|
   clean_up_for_winning_strategy = !warden.winning_strategy.respond_to?(:clean_up_csrf?) ||
     warden.winning_strategy.clean_up_csrf?
-  if Devise.clean_up_csrf_token_on_authentication && clean_up_for_winning_strategy
+  if Devise.clean_up_csrf_token_on_authentication && clean_up_for_winning_strategy && (options[:event] == :set_user || options[:event] == :authentication)
     if warden.request.respond_to?(:reset_csrf_token)
       # Rails 7.1+
       warden.request.reset_csrf_token
